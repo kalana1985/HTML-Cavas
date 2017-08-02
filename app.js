5df379928989cecf8c4450706e01e69f67cef25e
kalana1985 <kalana1on1@gmail.com>
2017-08-01 21:45:08 -0500

    making canvas 100% width and height

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('r/place');
+//createing canvas object
+var canvas = document.querySelector('canvas');
+// adding canvases height and width to 100%
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
