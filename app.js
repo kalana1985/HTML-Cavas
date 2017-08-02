84a5323c4b7ef8d1cb849e3d571bb7d08c99897b
kalana1985 <kalana1on1@gmail.com>
2017-08-01 23:28:02 -0500

    added some functions

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 //createing canvas object
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 // adding canvases height and width to 100%
 
 canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
+//create a new veriable call "c" and add getContext function to it
+//will use this const to drow the items to the canvas
+
+const c = canvas.getContext('2d');
+/*
+//in here we adding x , y codinets to the canves as well as the width and height.
+
+c.fillRect(x,y,width,height);
+
+*/
+
+c.fillRect(100,100,100,100);
+
+
+
